47dafd2b9f6c7e81a523d55a3ed5255f078f0a5a
Andrew <ajestkov@gmail.com>
2016-11-11 23:14:06 +0500

    go

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  var a = req.query.a ? req.query.a : 0;
+  var b = req.query.b ? req.query.b : 0;
+  a = parseInt(a);
+  b = parseInt(b);
+  console.log(a, b);
+  console.log(a + b);
+  res.send(200, a + b);
+
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
